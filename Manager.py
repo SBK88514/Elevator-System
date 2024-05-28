36a13be889d3c82694e5d0f9b830397e2a90f372
sbk88514 <sbk88514@gmail.com>
2024-05-28 10:49:26 +0300

    Adding a call to the elevator

--- Manager.py
+++ Manager.py
@@ -1,5 +1,3 @@
-import pygame
-
 from Building import Building
 from settings import *
 
@@ -15,22 +13,23 @@ class Manager(pygame.sprite.Sprite):
         pygame.init()
         self.clock.tick(FPS)
 
-    def check_click(self):
-        pass
+    def check_click(self, mouse_pos):
+        for building in self.buildings:
+            for floor in building.floors:
+                floor_clicked = floor.handle_click(mouse_pos)
+                if floor_clicked is not None:
+                    building.check_faster_elevator(floor_clicked)
+
+
+
 
     def create_buildings(self):
         for building_num in range(NUMBER_OF_BUILDING):
             x_position = FLOOR_WIDTH // 2 + building_num * (FLOOR_WIDTH + BUILDING_SPACING)
             building = Building(x_position=x_position)
-            #building_id = building_num
             self.buildings.append(building)
             self.add(building)
 
     def update(self):
         for building in self.buildings:
             building.update()
-        for building in self.buildings:
-            for floor in building.floors:
-                SCREEN.blit(floor.image, floor.rect)
-            for elevator in building.elevators:
-                SCREEN.blit(elevator.image, elevator.rect)
