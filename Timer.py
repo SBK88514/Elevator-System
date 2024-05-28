8d4c986263ffeb7cb01433d6632048b061713414
sbk88514 <sbk88514@gmail.com>
2024-05-28 10:56:03 +0300

    part of the class

---
+++ Timer.py
@@ -0,0 +1,23 @@
+import pygame
+from settings import TIMER_FONT_SIZE, SCREEN
+
+
+# from Floor import Floor
+
+
+class Timer(pygame.sprite.Sprite):
+    def __init__(self, position, initial_time=0):
+        super().__init__()
+        if not pygame.font.get_init():
+            pygame.font.init()
+        self.time_remaining = initial_time
+        self.position = position
+        self.font = pygame.font.SysFont(None, TIMER_FONT_SIZE)
+        self.text = self.font.render(str(self.time_remaining), True, (255, 255, 255))
+
+    def update(self, time_remaining):
+        self.time_remaining = time_remaining
+        self.text_surface = self.font.render(str(self.time_remaining), True, (255, 255, 255))
+
+    def draw(self, screen):
+        SCREEN.blit(self.text, self.position)
